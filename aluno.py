4b6298831adc75dd27e926af46f98801f2473d6b
gdarold <gadarold@gmail.com>
2019-08-30 21:53:35 -0300

    primeira versão

---
+++ aluno.py
@@ -0,0 +1,47 @@
+class Aluno:
+
+	notas = [4]
+	
+
+	media = 0
+        
+	
+
+def __init__(self, notas_aluno, nome_aluno):
+        self.notas = notas_aluno
+        self.aluno = nome_aluno
+        
+
+	
+
+def aprovado(self, media):
+        if (media)>=7:
+                print('aprovado')
+        elif (media)<4:
+                print ('reprovado')
+        else:
+                print ('em exame')
+
+
+def calcula_media(self):
+        for no in notas:
+                media = media+notas
+                
+        
+                
+
+if __name__ == '__main__':
+
+        nota = []
+        nome_aluno = "pedro"
+       
+        
+        for c in range(0,4):
+                nota.append(input('digite nota'))
+
+        print (nota)
+        
+        aluno1 = Aluno(media, nome_aluno)
+
+        aluno.calcula_media()
+        
